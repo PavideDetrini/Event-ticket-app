f36cedcbab8756c464c09d32c80eef52ddd538c0
PavideDetrini <davide.petrini05@gmil.com>
2024-04-25 14:15:38 +0200

    Aggiunto slider eventi

--- TicketApp/footer.php
+++ TicketApp/footer.php
@@ -13,28 +13,50 @@ if (!empty($pdo)) {
     }
 }
 
+/*
+  foreach ($categorie as $row){
+                    foreach ($row as $descrizione){
+                        ?>
+                <li class="nav-item">
+                    <a href="categoria.php?categoria=<?= $descrizione?>" class="nav-link"><?= $descrizione?></a>
+                </li>
+                <?php
+                    }
+                }
+ */
+
 ?>
 
 
-<footer class="text-center text-lg-start bg-body-tertiary text-muted">
-    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    </section>
 
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
     <section class="">
         <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
             <div class="row mt-3">
+                <!-- Grid column -->
                 <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
                     <h6 class="text-uppercase fw-bold mb-4">
-                        EventiNow
+                        <i class="fas fa-gem me-3"></i>EventiNow
                     </h6>
                     <p>
-                        Esplora un mondo di emozionanti esperienze su EventiNow, il tuo destino per eventi indimenticabili.<br>
-                        Scopri una vasta gamma di attività, concerti, film e molto altro ancora, tutto a portata di clic.<br>
-                        Affidati a noi per trasformare ogni momento in un'occasione memorabile.
+                        Esplora un mondo di emozionanti esperienze su EventiNow, il tuo destino per eventi indimenticabili. Scopri una vasta gamma di attività, concerti, mostre e molto altro ancora, tutto a portata di clic. Affidati a noi per trasformare ogni momento in un'occasione memorabile.
                     </p>
                 </div>
+                <!-- Grid column -->
 
+                <!-- Grid column -->
                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         Prodotti
                     </h6>
@@ -52,36 +74,48 @@ if (!empty($pdo)) {
                     }
                     ?>
 
-                </div>
 
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         Link utili
                     </h6>
                     <p>
-                        <a href="#" class="text-reset">Impostazioni</a>
+                        <a href="#!" class="text-reset">Impostazioni</a>
                     </p>
                     <p>
-                        <a href="#" class="text-reset">Ordini</a>
+                        <a href="#!" class="text-reset">Ordini</a>
                     </p>
                     <p>
                         <a href="help.php" class="text-reset">Supporto</a>
                     </p>
                 </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">Contatti</h6>
-                    <p><i class="bi bi-house-door-fill me-3"></i> New York, NY 10012, US</p>
+                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
                     <p>
-                        <i class="bi bi-envelope-fill me-3"></i>
+                        <i class="fas fa-envelope me-3"></i>
                         EventiNow@isisfacchinetti.edu.it
                     </p>
-                    <p><i class="bi bi-telephone-fill me-3"></i> + 01 234 567 88</p>
-                    <p><i class="bi bi-printer-fill me-3"></i> + 01 234 567 89</p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
                 </div>
+                <!-- Grid column -->
             </div>
+            <!-- Grid row -->
         </div>
     </section>
+    <!-- Section: Links  -->
 
+    <!-- Copyright -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
         <i class="bi bi-facebook"></i>
         <i class="bi bi-instagram"></i>
@@ -90,6 +124,37 @@ if (!empty($pdo)) {
         <i class="bi bi-linkedin"></i>
         <i class="bi bi-youtube"></i>
     </div>
+    <!-- Copyright -->
 </footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    $('.owl-carousel').owlCarousel({
+        margin: 10,
+        loop: true,
+        responsiveClass:true,
+        nav: true,
+        navText: [
+            "<i class='bi bi-arrow-left-circle-fill'></i>",
+            "<i class='bi bi-arrow-right-circle-fill'></i>"
+        ],
+        dots: false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:true
+            }
+        }
+    });
+</script>
 </body>
 </html>
--- TicketApp/index.php
+++ TicketApp/index.php
@@ -1,72 +1,95 @@
 <?php
-    include_once "header.php";
-    require "db_connection.php";
+include_once "header.php";
+require "db_connection.php";
 
-    $arrayCategorie = array();
-    if(isset($_SESSION['user'])){
-        //HOMEPAGE CONSIGLIATA
-        if(!empty($pdo)){
-            $query = "SELECT Eventi.Descrizione, Eventi.Prezzo, Eventi.ID_Evento , COUNT(Biglietti.ID_Biglietto) AS Biglietti_Venduti
-                      FROM Eventi LEFT JOIN Biglietti ON Eventi.ID_Evento = Biglietti.Evento
-                      GROUP BY Eventi.ID_Evento
-                      ORDER BY Biglietti_Venduti DESC LIMIT 9;";
+$arrayCategorie = array();
+if(isset($_SESSION['user'])){
+    //HOMEPAGE CONSIGLIATA
+    if(!empty($pdo)){
+        $query = "SELECT Eventi.Descrizione, Eventi.Prezzo, Eventi.ID_Evento , COUNT(Biglietti.ID_Biglietto) AS Biglietti_Venduti
+                  FROM Eventi LEFT JOIN Biglietti ON Eventi.ID_Evento = Biglietti.Evento
+                  GROUP BY Eventi.ID_Evento
+                  ORDER BY Biglietti_Venduti DESC LIMIT 9;";
 
-            $statement = $pdo -> query($query);
-            if(!$statement){
-                echo "ERROR";
-            }
-            else{
-                $eventiConsigliati = $statement -> fetchAll();
-                ?>
-                <h1>Eventi</h1>
-                <?php
-                foreach ($eventiConsigliati as $evento){
-                ?>
-                    <div>
-                        <img src="images\default.jpg">
-                        <a href="prodotto.php?id=<?=$evento['ID_Evento']?>"><?= $evento['Descrizione'] . ' ' .  $evento['Prezzo']?></a>
-                    </div>
-                <?php
-                }
-            }
-        }
-    }
-    if (!empty($pdo)) {
-        $query = "SELECT Descrizione_Categoria  FROM CategoriaEventi;";
         $statement = $pdo -> query($query);
-
-        if (!$statement){
+        if(!$statement){
             echo "ERROR";
         }
         else{
-            $categorie = $statement -> fetchAll();
-            foreach ($categorie as $value){
-                foreach ($value as $item){
-                    array_push($arrayCategorie, $item);
-                }
-            }
-        }
-    }
-
-    foreach ($arrayCategorie as $value){
-        array_push($arrayCategorie, $item);
-        $query3="SELECT Eventi.Descrizione, Eventi.Prezzo, Eventi.ID_Evento
-                 FROM Eventi JOIN CategoriaEventi ON Eventi.Categoria = CategoriaEventi.ID_Categoria
-                 WHERE CategoriaEventi.Descrizione_Categoria LIKE '$value';";
-
-        $statement3 = $pdo -> query($query3);
-        $eventi = $statement3 -> fetchAll();
-        ?>
-        <h1><?= $value?></h1>
-        <?php
-        foreach ($eventi as $evento){
+            $eventiConsigliati = $statement -> fetchAll();
             ?>
-            <div>
-                <img src="images\default.jpg">
-                <a href="prodotto.php?id=<?=$evento['ID_Evento']?>"><?= $evento['Descrizione'] . ' ' .  $evento['Prezzo']?></a>
+            <div class="container-slider">
+                <h1>Eventi Consigliati</h1>
+                <div class="owl-carousel owl-theme owl-loaded text-center">
+                    <div class="owl-stage-outer">
+                        <div class="owl-stage">
+                            <?php
+                            foreach ($eventiConsigliati as $evento){
+                                ?>
+                                <div class="owl-item">
+                                    <img src="images\default.jpg">
+                                    <a href="prodotto.php?id=<?=$evento['ID_Evento']?>"><?= $evento['Descrizione'] . ' ' .  $evento['Prezzo']?></a>
+                                </div>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
             <?php
         }
     }
+}
+if (!empty($pdo)) {
+    $query = "SELECT Descrizione_Categoria  FROM CategoriaEventi;";
+    $statement = $pdo -> query($query);
+
+    if (!$statement){
+        echo "ERROR";
+    }
+    else{
+        $categorie = $statement -> fetchAll();
+        foreach ($categorie as $value){
+            foreach ($value as $item){
+                array_push($arrayCategorie, $item);
+            }
+        }
+    }
+}
+
+foreach ($arrayCategorie as $value){
+    array_push($arrayCategorie, $item);
+    $query3="SELECT Eventi.Descrizione, Eventi.Prezzo, Eventi.ID_Evento
+             FROM Eventi JOIN CategoriaEventi ON Eventi.Categoria = CategoriaEventi.ID_Categoria
+             WHERE CategoriaEventi.Descrizione_Categoria LIKE '$value';";
+
+    $statement3 = $pdo -> query($query3);
+    $eventi = $statement3 -> fetchAll();
+    ?>
+    <div class="container-slider">
+        <div class="category-header">
+            <h1><?= $value?></h1>
+            <a href="categoria.php?categoria=<?=$value?>" class="read-more">Per saperne di più</a>
+        </div>
+        <div class="owl-carousel owl-theme owl-loaded text-center">
+            <div class="owl-stage-outer">
+                <div class="owl-stage">
+                    <?php
+                    foreach ($eventi as $evento){
+                        ?>
+                        <div class="owl-item">
+                            <img src="images\default.jpg">
+                            <a href="prodotto.php?id=<?=$evento['ID_Evento']?>"><?= $evento['Descrizione'] . ' ' .  $evento['Prezzo']?></a>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
 include_once "footer.php";
 ?>
